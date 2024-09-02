39af93f7681562c8b3624aa1af5901c156b9c1ca
DonavanGrobler <donavangrobler2@gmail.com>
2024-09-02 13:30:26 +0200

    Added GET requests

--- src/index.js
+++ src/index.js
@@ -4,6 +4,30 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.send({ msg: "Hello World <3" });
+});
+
+app.get("/api/users", (req, res) => {
+  res.send([
+    {
+      id: 1,
+      username: "donavan",
+      displayName: "Donavan",
+    },
+    {
+      id: 2,
+      username: "domenique",
+      displayName: "Domenique",
+    },
+    {
+      id: 1,
+      username: "caitlynn",
+      displayName: "Caitlynn",
+    },
+  ]);
+});
+
 app.listen(PORT, () => {
   console.log("Running Port:", PORT);
 });
